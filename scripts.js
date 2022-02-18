3cc3ff2fdf07e5301f6cf204dd7e8fd9ee27c38b
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-02-18 16:38:23 -0500

    schoolwork branched

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-//todo
-//fix constructor
-//figure out button thing
-
 titleinput = document.querySelector('#title')
 authorinput = document.querySelector('#author')
 readinput = document.querySelector('#read')
@@ -12,36 +8,26 @@ table = document.querySelector('table')
 
 let library = []
 
-function Book() {
+var Book = function(title, author, read) {
     this.title = title;
     this.author = author;
     this.read = read;
 }
 
+
 function addBook(title, author, read) {
-    let newBook = new Book(title, author, readinput.checked)
+    let newBook = new Book(title, author, read)
+    return newBook
 }
 
 submitbutton.addEventListener('click', () => {
     if (titleinput.value && authorinput.value) {
-        const BookAdd = addBook(titleinput.value, authorinput.value, readinput.checked)
-        library.push(this.Book)
+        let new1 = addBook(titleinput.value, authorinput.value, readinput.checked)
+        library.push(new1)
         console.log(library)
-        let newRow = document.createElement("tr")
-        let newTitle = document.createElement("td")
-        newTitle.textContent = titleinput.value
-        let newAuthor = document.createElement("td")
-        newAuthor.textContent = authorinput.value
-        let newRead = document.createElement("td")
-        let newButton = document.createElement("button")
-        newButton.textContent = readinput.checked
-        reset()
-        newRead.appendChild(newButton)
-        newRow.appendChild(newTitle)
-        newRow.appendChild(newAuthor)
-        newRow.appendChild(newRead)
-        table.appendChild(newRow)
+        createTable()
     }
+    console.table(library)
 })
 
 resetbutton.addEventListener('click', () => {
@@ -54,18 +40,70 @@ function reset() {
     readinput.checked = false
 }
 
+Book.prototype.changeRead = function() {
+    if (this.read == false) {
+        this.read = true
+    } else {
+        this.read = false
+    }
+};
 
-// let newRow = document.createElement("tr")
-// let newTitle = document.createElement("td")
-// newTitle.textContent = titleinput.value
-// let newAuthor = document.createElement("td")
-// newAuthor.textContent = authorinput.value
-// let newRead = document.createElement("td")
-// let newButton = document.createElement("button")
-// newButton.textContent = readinput.checked
-// reset()
-// newRead.appendChild(newButton)
-// newRow.appendChild(newTitle)
-// newRow.appendChild(newAuthor)
-// newRow.appendChild(newRead)
-// table.appendChild(newRow)
+function deleteEntry(library, title) {
+    let entry = findbook(library, title)
+    library.splice(entry, entry + 1)
+        //this only changes the library, not the webpage itself
+}
+
+function findBook(library, title) {
+    if (library.length === 0 || library === null) {
+        return;
+    }
+    for (title of library)
+        if (book.title === title) {
+            return library.indexOf(title);
+        }
+}
+
+function createTable() {
+    library.forEach((book) => {
+        let newRow = document.createElement("tr")
+        let newTitle = document.createElement("td")
+        newTitle.textContent = this.title.value
+        let newAuthor = document.createElement("td")
+        newAuthor.textContent = this.author.value
+        let newRead = document.createElement("td")
+        let newButton = document.createElement("button")
+        newButton.textContent = this[read]
+        newRead.appendChild(newButton)
+        newRow.appendChild(newTitle)
+        newRow.appendChild(newAuthor)
+        newRow.appendChild(newRead)
+        table.appendChild(newRow)
+    });
+}
+
+//currentTarget for the button can be used to navigate the nodse so hat you find the name of the book
+
+//adding a dataset to the button equal to the title could be an inelegant solution
+
+//once we have our thing it's is Book.changeread i think
+
+//how hou
+
+
+// old working table code 
+//         let newRow = document.createElement("tr")
+//         let newTitle = document.createElement("td")
+//         newTitle.textContent = titleinput.value
+//         let newAuthor = document.createElement("td")
+//         newAuthor.textContent = authorinput.value
+//         let newRead = document.createElement("td")
+//         let newButton = document.createElement("button")
+//         newButton.textContent = readinput.checked
+//       newButton.
+//         reset()
+//         newRead.appendChild(newButton)
+//         newRow.appendChild(newTitle)
+//         newRow.appendChild(newAuthor)
+//         newRow.appendChild(newRead)
+//         table.appendChild(newRow)
